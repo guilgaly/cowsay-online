a7fb3a80c9e737dc4c10b70453dd5a43acfa69ec
Guillaume Galy <guilgaly@gmail.com>
2019-03-28 09:51:41 +0100

    Make parser more flexible (parse options in any order)

--- server/test/src/cowsayonline/slack/model/TalkCommandTextParserSpec.scala
+++ server/test/src/cowsayonline/slack/model/TalkCommandTextParserSpec.scala
@@ -6,10 +6,10 @@ import cowsayonline.tests.UnitSpec
 class TalkCommandTextParserSpec extends UnitSpec {
   import TalkCommandText.ParsingError._
 
-  "The parser" when {
+  "The parser" should {
 
-    "given a command string without options" should {
-      "parse it" in {
+    "parse a command" when {
+      "given a command string without options" in {
         val text = "   Hello World!\n\tWhat a lovely test. ♥︎  "
         val expected = TalkCommandText.withDefaults(
           None,
@@ -17,10 +17,7 @@ class TalkCommandTextParserSpec extends UnitSpec {
           "Hello World!\n\tWhat a lovely test. ♥︎")
         TalkCommandText.Parser(text) shouldBe Right(expected)
       }
-    }
-
-    "given a string with options" should {
-      "parse it" in {
+      "given a string with options" in {
         val text =
           "cow=elephant-in-snake\nmode=borg  \t Hello World!\n\tWhat a lovely test. ♥︎  "
         val expected = TalkCommandText(
@@ -29,13 +26,21 @@ class TalkCommandTextParserSpec extends UnitSpec {
           "Hello World!\n\tWhat a lovely test. ♥︎")
         TalkCommandText.Parser(text) shouldBe Right(expected)
       }
+      "given a string with options in a different order" in {
+        val text =
+          "mode=stoned cow=pterodactyl Hello World!"
+        val expected = TalkCommandText(
+          DefaultCow.Pterodactyl,
+          DefaultCowMode.Stoned,
+          "Hello World!")
+        TalkCommandText.Parser(text) shouldBe Right(expected)
+      }
     }
 
-    "given a string with an illegal option" should {
-      "not parse it" in {
+    "reject a command" when {
+      "given a string with an illegal option" in {
         val text = "cow=toto mode=borg Hello."
         val expected = Left(List(InvalidCow("toto")))
-
         TalkCommandText.Parser(text) shouldBe expected
       }
     }
