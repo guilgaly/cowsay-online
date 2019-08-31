94312b560d2c4cdad1a27cbe5fdaa0a23ab9b549
Guillaume Galy <guilgaly@gmail.com>
2019-08-31 23:22:25 +0200

    Upgrade Heroku to JDK 12

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 if [[ $CIRCLE_BRANCH == "master" ]]; then
   echo "[INFO] Deploy JAR to Heroku"
-  heroku deploy:jar "out/server/assembly/dest/out.jar" --app "cowsay-online" --jdk 11
+  heroku deploy:jar "out/server/assembly/dest/out.jar" --app "cowsay-online" --jdk 12
 else
   echo "[INFO] No deployment to perform"
 fi
